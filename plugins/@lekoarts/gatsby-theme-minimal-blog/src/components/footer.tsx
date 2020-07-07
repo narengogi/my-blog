b80a7783c95a654d7b9d7162879183267079e01b
Narendra Nath Gogineni <47327611+segalord@users.noreply.github.com>
2020-07-07 13:09:54 +0530

    Add files via upload

---
+++ plugins/@lekoarts/gatsby-theme-minimal-blog/src/components/footer.tsx
@@ -0,0 +1,37 @@
+/** @jsx jsx */
+import { jsx, Link } from 'theme-ui';
+import useSiteMetadata from '../hooks/use-site-metadata';
+
+const Footer = () => {
+	const { siteTitle } = useSiteMetadata();
+
+	return (
+		<footer
+			sx={{
+				boxSizing: `border-box`,
+				display: `flex`,
+				justifyContent: `space-between`,
+				mt: [ 6 ],
+				color: `secondary`,
+				a: {
+					variant: `links.secondary`
+				},
+				flexDirection: [ `column`, `column`, `row` ],
+				variant: `dividers.top`
+			}}
+		>
+			<div>&copy; {new Date().getFullYear()} by Naren. </div>
+			<div>
+				Theme
+				{` `}
+				by
+				{` `}
+				<Link aria-label="Link to the theme author's website" href="https://www.lekoarts.de/en">
+					LekoArts
+				</Link>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
