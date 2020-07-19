9ce4c5787c89a6984ee245450446d551e6adaf98
Narendra Nath Gogineni <narenrockstar1@gmail.com>
2020-07-20 01:33:07 +0530

    Added Google analytics ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
 		{
 			resolve: `gatsby-plugin-google-analytics`,
 			options: {
-				trackingId: process.env.GOOGLE_ANALYTICS_ID
+				trackingId: 'UA-171747963-1'
 			}
 		},
 		`gatsby-plugin-sitemap`,
