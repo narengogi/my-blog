3991521bf754cd2d3c94941506c5c0b4be7c69a7
Narendra Nath Gogineni <narenrockstar1@gmail.com>
2021-02-19 15:25:36 +0530

    some posts

--- plugins/@lekoarts/gatsby-theme-minimal-blog/src/components/footer.tsx
+++ plugins/@lekoarts/gatsby-theme-minimal-blog/src/components/footer.tsx
@@ -11,21 +11,17 @@ const Footer = () => {
 				boxSizing: `border-box`,
 				display: `flex`,
 				justifyContent: `space-between`,
-				mt: [ 6 ],
+				mt: [6],
 				color: `secondary`,
 				a: {
 					variant: `links.secondary`
 				},
-				flexDirection: [ `column`, `column`, `row` ],
+				flexDirection: [`column`, `column`, `row`],
 				variant: `dividers.top`
 			}}
 		>
 			<div>&copy; {new Date().getFullYear()} by Naren. </div>
-			<div>
-				by
-				{` `}
-				Naren
-			</div>
+
 		</footer>
 	);
 };
