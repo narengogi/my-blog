65975bd1356e5e4107f2f7fae8b06932cd4a72b6
Narendra Nath Gogineni <nathsin@users.noreply.github.com>
2021-04-20 20:53:57 +0530

    Revert "fix?"
    This reverts commit 8653e062c98db0c6b5e333ab20fcd2434f517d82.
    
    revert fix

--- plugins/@lekoarts/gatsby-theme-minimal-blog/gatsby-config.js
+++ plugins/@lekoarts/gatsby-theme-minimal-blog/gatsby-config.js
@@ -12,7 +12,8 @@ module.exports = (options) => {
 			siteDescription: `Hey, I'm Narendra Nath Gogineni. and this is my personal website`,
 			siteLanguage: `en`,
 			siteImage: `/picture-of-naren-512x512.png`,
-			author: `Narendra Nath Gogineni`
+			author: `Narendra Nath Gogineni`,
+			title: `Naren's Space`
 		},
 		plugins: [
 			{
